90035cf8d6ff18cf6cabd16b6f980591170563dc
CircuitNeuroNet <141137353+Wenlun-Zhang@users.noreply.github.com>
2024-11-19 16:07:31 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -49,7 +49,7 @@ ASiM can be run with various devices and an estimation of running time (w/ full 
 
 ### 4.1. Abstract
 
-<a href="TODO" target="_blank">Paper Link</a>
+<a href="https://arxiv.org/abs/2411.11022" target="_blank">Paper Link</a>
 
 SRAM-based Analog Compute-in-Memory (ACiM) demonstrates promising energy efficiency for deep neural network (DNN) processing. Although recent aggressive design strategies have led to successive improvements on efficiency, there is limited discussion regarding the accompanying inference accuracy challenges. Given the growing difficulty in validating ACiM circuits with full-scale DNNs, standardized modeling methodology and open-source inference simulator are urgently needed. This paper presents ASiM, a simulation framework specifically designed to assess inference quality, enabling comparisons of ACiM prototype chips and guiding design decisions. ASiM works as a plug-and-play tool that integrates seamlessly with the PyTorch ecosystem, offering speed and ease of use. Using ASiM, we conducted a comprehensive analysis of how various design factors impact DNN inference. We observed that activation encoding can tolerate certain levels of quantization noise, indicating a substantial potential for bit-parallel scheme to enhance energy efficiency. However, inference accuracy is susceptible to noise, as ACiM circuits typically use limited ADC dynamic range, making even small errors down to 1 LSB significantly deteriorates accuracy. This underscores the need for high design standards, especially for complex DNN models and challenging tasks. In response to these findings, we propose two solutions: Hybrid Compute-in-Memory architecture and majority voting to secure accurate computation of MSB cycles. These approaches improve inference quality while maintaining ACiM’s energy efficiency benefits, offering promising pathways toward reliable ACiM deployment in real-world applications.
 
@@ -148,6 +148,95 @@ Here, a pretrained model weight must be given as `cfg.pretrain_model_path = 'mod
 | vitb32_cifar10_w8a8_trim_60.pkl          | CIFAR-10 | ViT-B-32 (W8A8)  | <a href="https://drive.google.com/file/d/1uypqLxoZA0hBX-UUOpk1-HsPAb3GDUqJ/view?usp=drive_link" target="_blank">Google Drive</a>         |
 | vitb32_imagenet_w8a8_trim_40.pkl         | ImageNet | ViT-B-32 (W8A8)  | <a href="https://drive.google.com/file/d/1-3togKT0lIMjk4Bhsb5jO_DoqaNbUhGI/view?usp=drive_link" target="_blank">Google Drive</a>         |
 
+## 5. Guiding Setup by Reproducing Results in the Paper
+
+In this section, we will give the setup guide by reproducing some results reported in the paper. Please follow the steps and familiarize how ASiM works.
+
+### 5.1. Prepare Environment and Datasets
+
+Please refer our software versions provided in Sec. 2 for environment settings. Install easydict if you do not have it, and download our model weights provided in Sec. 4.3.
+
+Download the following datasets: <a href="https://drive.google.com/file/d/1b7auKQzYE0VTe_0JCGCo8S6SzEl-WvuE/view?usp=drive_link" target="_blank">CIFAR-10</a>, <a href="https://drive.google.com/file/d/143wrVmpfqlH1Y0URvFVTFgyVhicO3ZA5/view?usp=drive_link" target="_blank">CIFAR-100</a>, and <a href="https://www.kaggle.com/c/imagenet-object-localization-challenge/overview/description" target="_blank">ImageNet</a>, respectively. Please use `./bins/valid_setup.py` to reformat the validation folder to match our dataset file of `./dataset/imagenet.py`.
+
+The recollected dataset path should align with the following form:
+
+CIFAR-10:
+
+```
+|-- CIFAR10
+|   |-- train
+|   |-- test
+|   |-- labels.txt
+```
+
+CIFAR-100:
+
+```
+|-- CIFAR100
+|   |-- train_images
+|   |-- test_images
+|   |-- train.csv
+|   |-- test.csv
+```
+
+ImageNet:
+
+```
+|-- ImageNet
+|   |-- train
+|   |   |-- n01440764
+|   |   |-- ...
+|   |-- valid
+|   |   |-- n01440764
+|   |   |-- ...
+```
+
+### 5.2. Simulation Examples
+
+#### 5.2.1. ResNet-18 on CIFAR-10
+
+For simulations of ResNet-18 on CIFAR-10, please refer configuration at `./examples/resnet18_cifar10/config.py`. Please give the dataset path of validation set to `cfg.test_dir`, and pre-trained model weight to `cfg.pretrain_model_path`. Based on your devices, please set the appropriate batch size and work loader to `cfg.test_bs` and `cfg.test_workers`.
+
+The default configuration is based on an 8b/8b model simulation on a bit-serial ACiM macro with row-parallelsim of 255. The CONV and Linear layer are configured separately. 
+
+The model quantization bit can be modified by `wbit` and `xbit` parameters. The row-parallelsim and ADC precision can be modified by `adc_prec` and `nrow` parameters. For analog noise injection, please give `rand_noise_sigma` and `non_linear_sigma` based on your Silicon measurement. For bit-parallel scheme simulation, configure `act_enc` parameter with the encoding bit width (2, 4, etc.). To verify the efficacy of HCiM, configure the `cfg.asim_cnn_hybrid_levels` parameter with hybrid boundary level (1, 2, 3, etc.) to prevent MSB cycles from noise impact.
+
+#### 5.2.2. ResNet-18 on ImageNet
+
+Similarly, please set dataset/model weight path and refer `./examples/resnet18_imagenet/config.py` for configurations on ImageNet.
+
+#### 5.2.3. ViT-B-32 on CIFAR-10
+
+In Transformer models, the configuration of MultiheadAttention, MLP, and FC layers are given separately. Notably, in MultiheadAttention layer, we further give individual settings to QK, AV, and WQKV/WO projection, respectively. Please note the Keys (K) and Activations in projection layers are have a sign bit that have to be processed individually in bit-serial manner, we set `attn_qk_kbit` and `attn_proj_xbit` to 9 so that the LSBs can be evenly divided by 2 and 4, respectively. The HCiM boundary level in ViT is in `cfg.asim_vit_hybrid_levels`. Please refer `./examples/vitb32_cifar10/config.py` for more details.
+
+#### 5.2.4. ViT-B-32 on ImageNet
+
+Similarly, please set dataset/model weight path and refer `./examples/vitb32_imagenet/config.py` for configurations on ImageNet.
+
+#### 5.2.5. Run Simulations
+
+After the above configuration settings, just run `./main/src_simulation.py` for ASiM simulations.
+
+### 5.3. Training Examples
+
+We give an example of training ResNet-18 model on CIFAR-10 dataset for ACiM applications.
+
+#### 5.3.1. Quantization-Aware Training (QAT)
+
+For model training, the `cfg.train_dir` and `cfg.valid_dir` have to be set as your dataset root. To perform training without an pre-trained model, just give `cfg.pretrain_model_path` a **None**. If you wish to start by a pre-trained state, please give the directory of pre-trained model weight. Please note that you may have to rearrange the parameters from original PyTorch provided models, in these cases, the state_dict have to be reformed (refer scripts in `./bins`). 
+
+Please set batch size and load workers by `cfg.train_bs`, `cfg.valid_bs`,  and `cfg.train_workers` respectively. Also, the `wbit` and `xbit` need to be configured to quantization weight and activation bit width, respectively.
+
+#### 5.3.2. Noise-Aware Training (NAT)
+
+For NAT, please give a pre-trained model to `cfg.pretrain_model_path`, and configure the noise intensity to `trim_noise` parameter.
+
+#### 5.3.3. Train Models
+
+After the above configuration settings, just run `./main/src_train.py` for model training.
+
+Kindly remind that training conditions such as epoch/learning rate may need to be configured case by case (`cfg.max_epoch` and `cfg.lr_init`). A `run` directory will be generated when the training is finished that containing all training log and model weights.
+
 ## Citation
 
 If you find this repo is useful, please cite our paper. Thanks.
@@ -156,6 +245,6 @@ If you find this repo is useful, please cite our paper. Thanks.
 @article{ASiM,
   title={ASiM: Improving Transparency of SRAM-based Analog Compute-in-Memory Research with an Open-Source Simulation Framework},
   author={Zhang, Wenlun and Ando, Shimpei and Chen, Yung-Chin and Yoshioka, Kentaro},
-  journal={arXiv},
+  journal={arXiv:2411.11022},
   year={2024}
 }
---
+++ examples/simulation/resnet18_cifar10/config.py
@@ -0,0 +1,281 @@
+"""
+
+Parameter management config for training/simulation.
+
+"""
+
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+from easydict import EasyDict
+
+# Obtain value via .key like key-value pair
+cfg = EasyDict()
+
+# <|Specify a task|>
+cfg.task = 'CIFAR-10'
+# cfg.task = 'CIFAR-100'
+# cfg.task = 'ImageNet'
+
+# <|Specify a model|>
+cfg.model_name = 'ResNet-18'
+# cfg.model_name = 'ResNet-34'
+# cfg.model_name = 'ResNet-50'
+# cfg.model_name = 'ResNet-101'
+# cfg.model_name = 'ResNet-152'
+# cfg.model_name = 'VGG11'
+# cfg.model_name = 'VGG13'
+# cfg.model_name = 'VGG16'
+# cfg.model_name = 'VGG19'
+# cfg.model_name = 'VGG11-BN'
+# cfg.model_name = 'VGG13-BN'
+# cfg.model_name = 'VGG16-BN'
+# cfg.model_name = 'VGG19-BN'
+# cfg.model_name = 'ViT-B-16'
+# cfg.model_name = 'ViT-B-32'
+# cfg.model_name = 'ViT-L-16'
+# cfg.model_name = 'ViT-L-32'
+# cfg.model_name = 'ViT-H-14'
+
+# <|Specify the input image size|>
+# CIFAR-10, CIFAR-100
+origin_size = 32
+input_size = 32
+# ImageNet and ViT
+# origin_size = 256
+# input_size = 224
+
+# <|Specify the running device: 'cpu', 'cuda', 'mps', etc.|>
+cfg.device = 'cuda'
+
+# <|Specify the classifier dimension|>
+cfg.cls_num = 10
+
+#############################################################
+################### <CNN module settings> ###################
+#############################################################
+
+# <|Specify using PACT or ReLU|>
+cfg.PACT = True
+# <|Specify Hybrid Computing Settings|>
+cfg.asim_cnn_hybrid_levels = None # Hybrid computing boundary level
+
+# <|Specify the settings for ASiMConv2d layers|>
+cfg.asim_conv_mode = 'Simulation' # Mode: Train/Inference/Simulation
+cfg.asim_conv_wbit = 8 # Weight bit
+cfg.asim_conv_xbit = 8 # Activation bit
+cfg.asim_conv_adc_prec = 8 # ADC precision
+cfg.asim_conv_nrow = 255 # CiM column length (number of row-parallelism)
+cfg.asim_conv_rand_noise_sigma = 0.0 # Standard deviation of random noise in %
+cfg.asim_conv_non_linear_sigma = 0.0 # Standard deviation of non-linear in %
+cfg.asim_act_enc = None # Activation encoding bit for bit-parallel computing (Give None for Bit-Serial Simulation)
+cfg.asim_signed_act = False # Signed or unsigned activation
+cfg.asim_conv_trim_noise = 0.0 # Noise intensity for noise-aware training or evaluate inference accuracy
+
+# <|Specify the settings for ASiMLinear layers|>
+cfg.asim_linear_mode = 'Simulation' # Mode: Train/Inference/Simulation
+cfg.asim_linear_wbit = 8 # Weight bit
+cfg.asim_linear_xbit = 8 # Activation bit
+cfg.asim_linear_adc_prec = 8 # ADC precision
+cfg.asim_linear_nrow = 255 # CiM column length (number of row-parallelism)
+cfg.asim_linear_rand_noise_sigma = 0.0 # Standard deviation of random noise in %
+cfg.asim_linear_non_linear_sigma = 0.0 # Standard deviation of non-linear in %
+cfg.asim_linear_act_enc = None # Activation encoding bit for bit-parallel computing (Give None for Bit-Serial Simulation)
+cfg.asim_linear_signed_act = False # Signed or unsigned activation
+cfg.asim_linear_layer = 'fc' # Layer type ('fc' (fully-connected) or 'proj' (projection))
+cfg.asim_linear_trim_noise = 0.0 # Noise intensity for noise-aware training or evaluate inference accuracy
+
+# <|Specify the settings for QuantConv2d layers|>
+cfg.quant_conv_mode = 'Train' # Mode: Train/Inference/Simulation
+cfg.quant_conv_wbit = 8 # Weight bit
+cfg.quant_conv_xbit = 8 # Activation bit
+
+#############################################################
+#############################################################
+#############################################################
+
+#############################################################
+################### <ViT module settings> ###################
+#############################################################
+
+# <|Input image size|>
+cfg.image_size = input_size
+# <|If use QuantConv2d to extract token embedding|>
+cfg.vit_quant_conv_proj = False
+# <|Specify Hybrid Computing Settings|>
+cfg.asim_vit_hybrid_levels = None # Hybrid computing boundary level
+
+# <|Specify the settings for ASiMMultiheadAttention layers|>
+cfg.asim_vit_attn_qk_qbit = 8 # Query bit in QK computation
+cfg.asim_vit_attn_qk_kbit = 8 # Key bit in QK computation
+cfg.asim_vit_attn_av_abit = 8 # Score bit in AV computation
+cfg.asim_vit_attn_av_vbit = 8 # Value bit in AV computation
+cfg.asim_vit_attn_proj_wbit = 8 # Weight bit in WX/WO projection
+cfg.asim_vit_attn_proj_xbit = 8 # Activation bit in WX/WO projection
+cfg.asim_vit_attn_qk_adc_prec = 8 # ADC precision in QK computation
+cfg.asim_vit_attn_av_adc_prec = 8 # ADC precision in AV computation
+cfg.asim_vit_attn_proj_adc_prec = 8 # ADC precision in WX/WO projection
+cfg.asim_vit_attn_nrow = 255 # CiM column length (number of row-parallelism)
+cfg.asim_vit_attn_qk_rand_noise_sigma = 0.0 # Standard deviation of random noise in % (QK computation)
+cfg.asim_vit_attn_av_rand_noise_sigma = 0.0 # Standard deviation of random noise in % (AV computation)
+cfg.asim_vit_attn_proj_rand_noise_sigma = 0.0 # Standard deviation of random noise in % (WQ/WK/WV/WO computation)
+cfg.asim_vit_attn_qk_non_linear_sigma = 0.0 # Standard deviation of non-linear in % (QK computation)
+cfg.asim_vit_attn_av_non_linear_sigma = 0.0 # Standard deviation of non-linear in % (AV computation)
+cfg.asim_vit_attn_proj_non_linear_sigma = 0.0 # Standard deviation of non-linear in % (WQ/WK/WV/WO computation)
+cfg.asim_vit_attn_qk_k_enc = None # Key encoding bit for bit-parallel computing (Give None for Bit-Serial Simulation)
+cfg.asim_vit_attn_av_a_enc = None # Score encoding bit for bit-parallel computing (Give None for Bit-Serial Simulation)
+cfg.asim_vit_attn_proj_act_enc = None # Activation encoding bit for bit-parallel computing (Give None for Bit-Serial Simulation)
+cfg.asim_vit_attn_mode = 'Train' # Mode: Train/Inference/Simulation
+cfg.asim_vit_attn_attn_trim_noise = 0.0 # Noise intensity for noise-aware training or evaluate inference accuracy (Attention)
+cfg.asim_vit_attn_proj_trim_noise = 0.0 # Noise intensity for noise-aware training or evaluate inference accuracy (Projection)
+
+# <|Specify the settings for MLP layers|>
+cfg.asim_vit_mlp_wbit = 8 # Weight bit
+cfg.asim_vit_mlp_xbit = 8 # Activation bit
+cfg.asim_vit_mlp_adc_prec = 8 # ADC precision
+cfg.asim_vit_mlp_nrow = 255 # CiM column length (number of row-parallelism)
+cfg.asim_vit_mlp_rand_noise_sigma = 0.0 # Standard deviation of random noise in %
+cfg.asim_vit_mlp_non_linear_sigma = 0.0 # Standard deviation of non-linear in %
+cfg.asim_vit_mlp_act_enc = None # Activation encoding bit for bit-parallel computing (Give None for Bit-Serial Simulation)
+cfg.asim_vit_mlp_mode = 'Train' # Mode: Train/Inference/Simulation
+cfg.asim_vit_mlp_trim_noise = 0.0 # Noise intensity for noise-aware training or evaluate inference accuracy
+
+# <|Specify the settings for FC layer|>
+cfg.asim_vit_fc_wbit = 8 # Weight bit
+cfg.asim_vit_fc_xbit = 8 # Activation bit
+cfg.asim_vit_fc_adc_prec = 8 # ADC precision
+cfg.asim_vit_fc_nrow = 255 # CiM column length (number of row-parallelism)
+cfg.asim_vit_fc_rand_noise_sigma = 0.0 # Standard deviation of random noise in %
+cfg.asim_vit_fc_non_linear_sigma = 0.0 # Standard deviation of non-linear in %
+cfg.asim_vit_fc_act_enc = None # Activation encoding bit for bit-parallel computing (Give None for Bit-Serial Simulation)
+cfg.asim_vit_fc_mode = 'Train' # Mode: Train/Inference/Simulation
+cfg.asim_vit_fc_trim_noise = 0.0 # Noise intensity for noise-aware training or evaluate inference accuracy
+
+# <|Specify the settings for QuantConv2d layers|>
+cfg.asim_vit_quant_conv_wbit = 8 # Weight bit
+cfg.asim_vit_quant_conv_xbit = 8 # Activation bit
+cfg.asim_vit_quant_conv_mode = 'Train' # Mode: Train/Inference/Simulation
+
+#############################################################
+#############################################################
+#############################################################
+
+# <|Specify the directory for training/validation/test(simulation) dataset|>
+# CIFAR-10
+cfg.train_dir = r'D:\Machine_Learning\Dataset\CIFAR10\train'
+cfg.valid_dir = r'D:\Machine_Learning\Dataset\CIFAR10\test'
+cfg.test_dir = r'D:\Machine_Learning\Dataset\CIFAR10\test'
+# CIFAR-100
+# cfg.train_dir = r'E:\Machine_Learning\Dataset\Processing\Cifar100\train_images'
+# cfg.valid_dir = r'E:\Machine_Learning\Dataset\Processing\Cifar100\test_images'
+# cfg.test_dir = r'E:\Machine_Learning\Dataset\Processing\Cifar100\test_images'
+# ImageNet
+# cfg.train_dir = r'E:\Machine_Learning\Dataset\Processing\ImageNet\imagenet-object-localization-challenge\ILSVRC\Data\CLS-LOC\train'
+# cfg.valid_dir = r'E:\Machine_Learning\Dataset\Processing\ImageNet\imagenet-object-localization-challenge\ILSVRC\Data\CLS-LOC\valid'
+# cfg.test_dir = r'E:\Machine_Learning\Dataset\Processing\ImageNet\imagenet-object-localization-challenge\ILSVRC\Data\CLS-LOC\valid'
+
+# <|Specify the pretrained model for noise-aware training or test (simulation)|>
+# cfg.pretrain_model_path = None  # For training, give a None.
+cfg.pretrain_model_path = r'D:\Machine_Learning\ASiM_Models\resnet18_cifar10_w8a8_pact_trim_100.pkl'
+
+# <|Specify the batch size and workers for training|>
+cfg.train_bs = 128
+cfg.valid_bs = 128
+cfg.train_workers = 8
+
+# <|Specify the batch size and workers for test (simulation)|>
+cfg.test_bs = 512
+cfg.test_workers = 8
+
+###############################################################
+##################### <Training settings> #####################
+###############################################################
+
+# Settings for initial training
+cfg.max_epoch = 200 # Training = 200, Fine-Tune = 80
+cfg.lr_init = 0.01 # SGD Training = 0.01, SGD Fine-Tune = 0.001, Adam Training = 1e-4, Adam Fine-Tune = 1e-5
+cfg.milestones = [int(cfg.max_epoch * 0.6), int(cfg.max_epoch * 0.9)]
+cfg.mixup = False    # Use mixup or not.
+cfg.label_smooth = False # Use label smoothing or not.
+
+###############################################################
+###############################################################
+###############################################################
+
+
+#####################################################################################################################
+# No need to modify the following params in general
+#####################################################################################################################
+
+# <|Specify the optimizer parameters for training/noise-aware training|>
+cfg.momentum = 0.9
+cfg.weight_decay = 1e-4
+cfg.factor = 0.1    # gamma (lr decay param) when using MultiStepLR
+cfg.log_interval = 10 # Training log interval
+cfg.mixup_alpha = 1.0    # Mixup parameter of beta distribution.
+cfg.label_smooth_eps = 0.01 # Label smoothing eps parameter.
+
+assert cfg.task in ['CIFAR-10', 'CIFAR-100', 'ImageNet'], "Invalid task specified. Choose from 'CIFAR-10', 'CIFAR-100', or 'ImageNet'."
+if cfg.task == 'CIFAR-10':
+    norm_mean = [0.4914, 0.4822, 0.4465]  # CIFAR-10
+    norm_std = [0.2470, 0.2435, 0.2616]  # CIFAR-10
+if cfg.task == 'CIFAR-100':
+    norm_mean = [0.5071, 0.4867, 0.4408]    # CIFAR-100
+    norm_std = [0.2675, 0.2565, 0.2761]     # CIFAR-100
+if cfg.task == 'ImageNet':
+    norm_mean = [0.485, 0.456, 0.406]  # ImageNet
+    norm_std = [0.229, 0.224, 0.225]  # ImageNet
+
+cfg.transforms_train = transforms.Compose([
+    transforms.RandomChoice(
+        [
+            transforms.ColorJitter(brightness=0.5),
+            transforms.ColorJitter(contrast=0.5),
+            transforms.ColorJitter(saturation=0.5),
+            transforms.ColorJitter(hue=0.3)
+        ]
+    ),
+    transforms.Resize((origin_size, origin_size)),
+    transforms.CenterCrop(origin_size),
+    transforms.RandomCrop(input_size, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15),
+    transforms.ToTensor(),
+    transforms.Normalize(norm_mean, norm_std)
+])
+
+cfg.transforms_valid = transforms.Compose([
+    transforms.Resize((input_size, input_size)),
+    transforms.ToTensor(),
+    transforms.Normalize(norm_mean, norm_std)
+])
+
+cfg.transforms_test = transforms.Compose([
+    transforms.Resize((input_size, input_size)),
+    transforms.ToTensor(),
+    transforms.Normalize(norm_mean, norm_std)
+])
+
+if input_size > 100:
+    cfg.large_model = True
+else:
+    cfg.large_model = False
+
+if __name__ == '__main__':      # Testbench
+
+    from dataset.cifar100 import Cifar100Dataset
+    from torch.utils.data import DataLoader
+    from tools.common_tools import inverse_transform
+    train_data = Cifar100Dataset(root_dir=cfg.train_dir, transform=cfg.transforms_train)
+    train_loader = DataLoader(dataset=train_data, batch_size=cfg.train_bs, shuffle=True)
+
+    for epoch in range(cfg.max_epoch):
+        for i, data in enumerate(train_loader):
+
+            inputs, labels, dir = data       # B C H W
+
+            img_tensor = inputs[0, ...]     # C H W
+            img = inverse_transform(img_tensor, cfg.transforms_train)
+            plt.imshow(img)
+            plt.show()
+            plt.pause(0.5)
+            plt.close()
